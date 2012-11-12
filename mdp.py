011e357dee684830bcc0f63af17ec5b664d75438
David Pfau <david.pfau@gmail.com>
2012-11-12 16:30:05 -0500

    Rewrote MDP class so that general gridworlds are easy(er) to construct.

--- mdp.py
+++ mdp.py
@@ -14,78 +14,119 @@ class MDP(object):
 		''' return set of allowed actions '''
 		return self.action_set
 
-	def act(self,action):
+	def act(self, action):
 		''' update state, return reward '''
 		pass
 
-class TwoRooms(MDP):
-	''' A gridworld consisting of two rooms, each of the same height and width
-	    with a door connecting them at half the height of the room.  The state
-	    space is represented as (height, width, room), where room is 0 or 1 in
-	    a room, and -1 in the doorway between them, in which case the height
-	    and width are 0 by (my totally arbitrary) convention. '''
-
+class Grid(MDP):
 	self.action_set = set(['u','d','l','r'])
 
-	def __init__(self, state, h=10, w=10):
+	def __init__(self, state=(0,0), h=10, w=10):
 		self.h = int(h)
 		self.w  = int(w)
 		self.state = state
-		x, y, r = self.state
-		if x < 0 or x >= h\
-	    or y < 0 or y >= w\
-	    or r < -1 or r > 1\
-	    or (r == 0 and (x != 0 or y != 0)):
-	    	raise Error('State is out of allowed range') 
+		x, y = self.state
+		assert x >=0 and x < self.h and y >= 0 and y < self.w, 'State is out of allowed range: x = %i y = %i' % self.state
 
-	def act(self,action):
+	def act(self, action):
 		if action in self.action_set:
-			x, y, r = self.state
-			if r == -1:
-				if action is 'l':
-					self.state = self.h/2, self.w-1, 0
+			x, y = self.state
+			if action is 'u':
+				if x is not 0:
+					self.state = x-1, y
 					return -1
-				elif action is 'r':
-					self.state = self.h/2, 0, 1
+				else:
+					return -10
+			elif action is 'l':
+				if y is not 0:
+					self.state = x, y-1
+					return -1
+				else:
+					return -10
+			elif action is 'r':
+				if y is not self.w-1:
+					self.state = x, y+1
 					return -1
 				else:
 					return -10
 			else:
-				if action is 'l':
-					if y > 0:
-						self.state = x, y-1, r
-						return -1
-					elif r is 1 and x is self.h/2:
-						self.state = 0, 0, -1
-						return -1
-					else:
-						return -10
-				if action is 'r':
-					if y < self.w-1:
-						self.state = x, y+1, r
-						return -1
-					elif r is 0 and x is self.h/2:
-						self.state = 0, 0, -1
-						return -1
-					else:
-						return -10
-				if action is 'u':
-					if x > 0:
-						self.state = x-1, y, r
-						return -1
-					else:
-						return -10
-				if action is 'd':
-					if x < self.h-1:
-						self.state = x+1, y, r
-						return -1
-					else:
-						return -10
+				if x is not self.h-1:
+					self.state = x+1, y
+					return -1
+				else:
+					return -10
 		else:
-			raise Error('Action not in allowed set') 
+			raise Error('Action not in allowed set') 		
+
+class GridWorld(MDP):
+	''' Grid world environment, made up of list of Grid objects, as well as a list of borders, 
+	    which map a particular point on the boundary to a Grid object and state tuple for that
+	    Grid object. '''
+
+	self.action_set = set(['u','d','l','r'])
+	self.dir_to_idx = {'u':0, 'r':1, 'd':2, 'l':3} # map direction to index of 'borders' tuple corresponding to that border
+
+	def __init__(self, state=(0,0,0), grids=[Grid()], borders=None):
+		idx, x, y = self.state
+		self.state = grids[idx], x, y
+		self.grids = grids
+		if borders is None:
+			for grid in self.grids
+				grid.borders = (grid.w*[None], grid.h*[None], grid.w*[None], grid.h*[None]) # N, E, W, S border
+		else:
+			assert len(self.grids) == len(borders), 'Borders and grids must be one-to-one'
+			for i in range(len(self.grids)):
+				self.grids[i].borders = borders[i]
+
+	def act(self, action):
+		if action in self.action_set:
+			grid, x, y = self.state
+			r = grid.act(action)
+			if r is -10: # if we hit the border, check if we go to another grid
+				border = grid.borders[self.dir_to_idx[action]]
+				if border is None: # Nope, just hit a wall
+					return r
+				else:
+					x, y = border[1]
+					border[0].state = x, y
+					self.state = border[0], x, y
+					return -1
+			else:
+				return r
+		else:
+			raise Error('Action not in allowed set') 		
+
+class TwoRooms(GridWorld):
+	''' A gridworld consisting of two rooms, each of the same height and width
+	    with a door connecting them at half the height of the room. '''
+
+	def __init__(self, state=(0,0), h=10, w=10):
+		x, y = state
+		left  = Grid((x,y), h, w)
+		right = Grid((0,0), h, w)
+		link  = Grid((0,0), 1, 1)
+		leftborder  = [w*[None], h*[None], w*[None], h*[None]]
+		leftborder[1][h/2] = (link,(0,0))
+		rightborder = [w*[None], h*[None], w*[None], h*[None]]
+		rightborder[3][h/2] = (link,(0,0))
+		linkborder  = ([None], [(right,(h/2,0))], [None], [(left,(h/2,w-1))])
+		GridWorld.__init__(self, (0,x,y), [left, right, link], [tuple(leftborder), tuple(rightborder), tuple(link)])
 
 class Taxi(MDP):
-	pass
+	# Actions:
+	# u - up
+	# d - down
+	# l - left
+	# r - right
+	# p - pickup
+	# q - drop off (quit?)
+	self.action_set = set(['u','d','l','r','p','q'])
+
+	def __init__(self, state, goal):
+		
+
+	def act(self,action):
+
 
 class Hanoi(MDP):
 	pass
